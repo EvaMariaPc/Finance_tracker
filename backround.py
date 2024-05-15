a5f5490adc02a502d1fa0c50c23304d345b1770f
EvaMariaPc <167923103+EvaMariaPc@users.noreply.github.com>
2024-05-15 12:42:34 +0300

    .

--- backround.py
+++ backround.py
@@ -1,19 +1,1 @@
-import streamlit as st
 
-
-class Backround:
-    def _init_(self):
-        pass
-    backround = []
-
-
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #FFC0CB;  /* Pink */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
